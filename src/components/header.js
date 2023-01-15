42b7816106da92e9eba62811b3857cd4243a99de
jomonty <joshmontgomery@protonmail.com>
2023-01-15 16:16:56 +0000

    cleaned out folder, added under construction details

---
+++ src/components/header.js
@@ -0,0 +1,8 @@
+
+const Header = () => {
+    return (
+        <h1 className="App-header">Jomonty</h1>
+    )
+};
+
+export default Header
---
+++ src/components/main.js
@@ -0,0 +1,8 @@
+
+const Main = () => {
+    return (
+        <h2 className="App">Under Construction</h2>
+    )
+};
+
+export default Main
