a413a400a1116c0872d76446da9083fb9b6519ab
jomonty <100322748+jomonty@users.noreply.github.com>
2023-01-16 09:54:14 +0000

    replaced browser router with hashrouter

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Nav from './components/Nav';
 import Home from './components/Home';
 
 function App() {
   return (
     <div>
-      <Router>
+      {/* <Router> */}
         <Nav />
         <Routes>
           <Route path="/" element={ <Home /> } />
         </Routes>
-      </Router>
+      {/* </Router> */}
     </div>
   );
 }
